191524bee5944c6dd97e6e00569c0c73abdf3e6f
Daniel Foerster <pydsigner@gmail.com>
2015-05-25 17:11:22 -0500

    Re-added LoF tile-art
    * Returned the custom Hurnscald look
    * A couple additions to the desert2 and inn2 tilesets
    * Added new tilesets christmas-decor, woodland_indoor_x3_extra, and woodland_x5_fall with Christmas 2013 tiles

---
+++ tilesets/christmas-decor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Christmas Decor" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/christmas_decor.png" width="512" height="608"/>
+</tileset>
---
+++ tilesets/woodland_indoor_x3_extra.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="woodland_indoor_x3_extra" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/woodland_indoor_x3_extra.png" width="512" height="96"/>
+</tileset>
