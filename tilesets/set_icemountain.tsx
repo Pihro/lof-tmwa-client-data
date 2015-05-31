cd8e66a66c74c9e9b35fe7bd7d5613f4317141d6
Daniel Foerster <pydsigner@gmail.com>
2015-05-30 21:58:39 -0500

    More tilesets
    * Added tileset information for several unused tilesets
    * Added a few tiles needed for Heroes' Hold to castle2

---
+++ tilesets/set_icemountain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Ice Mountain" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/set_icemountain.png" width="224" height="128"/>
+</tileset>
---
+++ tilesets/snow_ground_x2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="snow_ground_x2" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/snow_ground_x2.png" width="256" height="160"/>
+</tileset>
