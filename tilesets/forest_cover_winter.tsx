527ac7019c4b66e2795a2344dcab736b05cfb4c3
Bill <wreese01@tampabay.rr.com>
2015-12-10 16:51:17 -0500

    Minor map errors that KeeKeeKat and demure found

---
+++ tilesets/forest_cover_winter.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="forest_cover_winter" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/forest_cover_winter.png" width="320" height="320"/>
+</tileset>
