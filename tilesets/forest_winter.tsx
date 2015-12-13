4c6b1964bf56c114fe963dba25780099cb2f1db7
Bill <wreese01@tampabay.rr.com>
2015-12-13 13:37:46 -0500

    Forest winter tileset tsx file

---
+++ tilesets/forest_winter.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="forest_winter" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/forest_winter.png" width="608" height="672"/>
+</tileset>
