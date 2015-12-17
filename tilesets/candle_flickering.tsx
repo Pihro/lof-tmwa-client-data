17afddb02e1cb65489388ca340e73022c165d26b
Bill <wreese01@tampabay.rr.com>
2015-12-17 00:13:40 -0500

    Small changes KeeKee requested
    Plus a small flickering candle

---
+++ tilesets/candle_flickering.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="candle_flickering" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/candle_flickering.png" width="160" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="130"/>
+   <frame tileid="1" duration="130"/>
+   <frame tileid="2" duration="130"/>
+   <frame tileid="3" duration="130"/>
+   <frame tileid="4" duration="130"/>
+  </animation>
+  <properties>
+   <property name="animation-delay0" value="13"/>
+   <property name="animation-delay1" value="20"/>
+   <property name="animation-delay2" value="13"/>
+   <property name="animation-delay3" value="13"/>
+   <property name="animation-delay4" value="13"/>
+   <property name="animation-frame0" value="0"/>
+   <property name="animation-frame1" value="1"/>
+   <property name="animation-frame2" value="2"/>
+   <property name="animation-frame3" value="3"/>
+   <property name="animation-frame4" value="4"/>
+  </properties>
+ </tile>
+</tileset>
