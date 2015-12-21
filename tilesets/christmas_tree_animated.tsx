2dfb1c586dd13a354a1a3d9d499d4c849ef2fe5b
Bill <wreese01@tampabay.rr.com>
2015-12-21 12:42:42 -0500

    Added animated Christmas palm tree
    Added Christmas Palm Tree to 001-1, modified animated Christmas Tree, updated license.txt

--- tilesets/christmas_tree_animated.tsx
+++ tilesets/christmas_tree_animated.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="christmas_tree_animated" tilewidth="128" tileheight="192" tilecount="5">
- <image source="../graphics/tiles/christmas_tree_animated.png" width="640" height="192"/>
+<tileset name="christmas_tree_animated" tilewidth="128" tileheight="160" tilecount="10">
+ <image source="../graphics/tiles/christmas_tree_animated.png" width="640" height="320"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="260"/>
@@ -8,6 +8,15 @@
    <frame tileid="2" duration="320"/>
    <frame tileid="3" duration="260"/>
    <frame tileid="4" duration="320"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="260"/>
+   <frame tileid="6" duration="320"/>
+   <frame tileid="7" duration="320"/>
+   <frame tileid="8" duration="260"/>
+   <frame tileid="9" duration="320"/>
   </animation>
  </tile>
 </tileset>
---
+++ tilesets/palm_lights.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="palm lights" tilewidth="96" tileheight="128">
+ <image source="../graphics/tiles/palm_lights.png" width="384" height="256"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="320"/>
+   <frame tileid="1" duration="320"/>
+   <frame tileid="2" duration="320"/>
+   <frame tileid="3" duration="320"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="320"/>
+   <frame tileid="5" duration="320"/>
+   <frame tileid="6" duration="320"/>
+   <frame tileid="7" duration="320"/>
+  </animation>
+ </tile>
+</tileset>
